557ab09e779c3b4065b90d0fcd2b7d5cce3d87af
BenWithVees <nukelaloosh06@gmail.com>
2016-04-05 12:01:23 +0900

    Added new fields. @JsonIgnore on password for JSON format

--- rabbitmq/BootApp/src/main/java/com/markham/main/User.java
+++ rabbitmq/BootApp/src/main/java/com/markham/main/User.java
@@ -7,6 +7,8 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "users")
 @NamedQueries({ @NamedQuery(name = "User.findUserInfo", query = "SELECT u FROM User u WHERE u.username =:username"),
@@ -20,6 +22,10 @@ public class User {
 
 	@Column(name = "username")
 	private String username;
+
+	@JsonIgnore
+	@Column(name = "password")
+	private String password;
 
 	@Column(name = "email")
 	private String email;
@@ -36,6 +42,14 @@ public class User {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getEmail() {
